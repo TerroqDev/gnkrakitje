10b4656b210a93a80b1d3bf7a8f2d120bf5e0ced
valentin994 <valentin.vareskic@gmail.com>
2025-03-27 10:02:49 +0100

    trigger cf

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,4 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 <h1 class="text-9xl">HELLLOOOOOOOOO</h1>
+<h2>eshfs</h2>
